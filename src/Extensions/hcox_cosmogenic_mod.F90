cf0b15d76a83cb24b2ff9d32ddd43a7e34d40810
Lee T. Murray <lee.murray@rochester.edu>
2025-07-30 13:05:17 -0400

    Revert earlier HEMCO messaging

--- src/Extensions/hcox_cosmogenic_mod.F90
+++ src/Extensions/hcox_cosmogenic_mod.F90
@@ -396,14 +396,18 @@ CONTAINS
 
        ! Write the name of the extension regardless of the verbose setting
        msg = 'Using HEMCO extension: Cosmogenic (radionuclide emissions)'
-       CALL HCO_Msg( msg, sep1='-', LUN=HcoState%Config%hcoLogLUN ) ! with separator
+       IF ( HCO_IsVerb( HcoState%Config%Err ) ) THEN
+          CALL HCO_Msg( HcoState%Config%Err, sep1='-' ) ! with separator
+       ELSE
+          CALL HCO_Msg( msg, verb=.TRUE.              ) ! w/o separator
+       ENDIF
 
        ! Write all other messages as debug printout only
        MSG = 'Use the following species (Name: HcoID):'
-       CALL HCO_MSG(MSG,LUN=HcoState%Config%hcoLogLUN)
+       CALL HCO_MSG(HcoState%Config%Err,MSG)
        DO N = 1, nSpc
           WRITE(MSG,*) TRIM(SpcNames(N)), ':', HcoIDs(N)
-          CALL HCO_MSG(MSG,LUN=HcoState%Config%hcoLogLUN)
+          CALL HCO_MSG(HcoState%Config%Err,MSG)
        ENDDO
     ENDIF
 
